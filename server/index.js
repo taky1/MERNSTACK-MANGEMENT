056b16495f509b1dc08149bfa9dd731b0c6d4f91
taky1 <48655485+taky1@users.noreply.github.com>
2021-01-22 01:00:32 +0100

    secondCommit

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+import postRouter from './route/Posts.js';
+const app = express();
+
+app.use('/Posts', postRouter);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://takieddine:takieddine@cluster0.v5fgh.mongodb.net/Processus?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`) ))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
